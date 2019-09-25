14fe43e24cf174c5eebff46801426e1937b4ef87
Dima Bakhitov <bakhitovd@gmail.com>
2019-09-25 22:06:07 +0500

    coment

--- table_csv_reader.py
+++ table_csv_reader.py
@@ -3,12 +3,15 @@ from datetime import datetime, timedelta, date
 from classes import WellsStartDates
 wells = {}
 with open('merfond.csv', 'r', encoding = 'utf-8') as f:
-    mer = csv.reader(f, delimiter = ';')
-    for line in mer:
-        print(' ', line)
+    mer = csv.DictReader(f, delimiter = ';')
+    print(next(mer))
+    #header = mer[0]
+    
+    #for line in mer:
+    #    print(line)
     #    well_id = line.SK_1
-     #   well_name = line.S1_1
-     #   wells.append({                                #добавление словаря в список словарей
+    #    well_name = line.S1_1
+    #    wells.append({                                #добавление словаря в список словарей
      #           'well_id' : well_id,
      #           'well_name' : well_name
      #       })
