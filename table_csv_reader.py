0edc90fadbcffca531f510798e534186bb203dbc
Dima Bakhitov <bakhitovd@gmail.com>
2019-09-26 11:34:27 +0500

    поптка сделать список словарей

--- table_csv_reader.py
+++ table_csv_reader.py
@@ -5,13 +5,13 @@ wells = {}
 with open('merfond.csv', 'r', encoding = 'utf-8') as f:
     mer = csv.DictReader(f, delimiter = ';')
     print(next(mer))
-    #header = mer[0]
-    
-    #for line in mer:
-    #    print(line)
-    #    well_id = line.SK_1
-    #    well_name = line.S1_1
-    #    wells.append({                                #добавление словаря в список словарей
-     #           'well_id' : well_id,
-     #           'well_name' : well_name
-     #       })
+    for line in mer:
+        #print(line['SK_1'])
+        #print(line['S1_1'])
+        well_id = line['SK_1']
+        well_name = line['S1_1']
+        wells.append({                                #добавление словаря в список словарей
+                'well_id' : well_id,
+                'well_name' : well_name
+            })
+
