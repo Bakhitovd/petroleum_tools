458c7a19d3dc4a37bde84c6a041b24547959d4b8
Dima Bakhitov <bakhitovd@gmail.com>
2019-09-26 20:11:16 +0500

    начинаю работу с sqlalchemy

---
+++ create_fake_db.py
@@ -0,0 +1,2 @@
+from sql_al import db
+db.create_all()
--- table_csv_reader.py
+++ table_csv_reader.py
@@ -1,10 +1,10 @@
 import csv
 from datetime import datetime, timedelta, date
 from classes import WellsStartDates
-wells = {}
+wells = []
 with open('merfond.csv', 'r', encoding = 'utf-8') as f:
     mer = csv.DictReader(f, delimiter = ';')
-    print(next(mer))
+    #print(next(mer))
     for line in mer:
         #print(line['SK_1'])
         #print(line['S1_1'])
@@ -15,3 +15,4 @@ with open('merfond.csv', 'r', encoding = 'utf-8') as f:
                 'well_name' : well_name
             })
 
+print (wells)
