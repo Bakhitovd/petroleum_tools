1660c042dad0b54ba919610de6211f666b79f150
Dima Bakhitov <bakhitovd@gmail.com>
2019-10-09 15:08:05 +0500

    создание графиков вынесено в функцию

--- get_data_from_db.py
+++ get_data_from_db.py
@@ -1,5 +1,4 @@
 from model import db, WellMonthRates, Base, WellWorkDates, WellFormation, WellPad, WellTRPressure, WellBHPPressure
-import csv
 from datetime import datetime, timedelta
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy import create_engine, func
@@ -13,19 +12,25 @@ field_oil = []
 field_gas = []
 dates = []
 
-for row in session.query(WellMonthRates.date, func.sum(WellMonthRates.oil)).group_by(WellMonthRates.date):
+def gaf_creation(x,y,name):
+    fig = go.Figure()
+    fig.add_trace(go.Scatter(
+        x=x,
+        y=y,
+        name = name, # Style name/legend entry with html tags
+        connectgaps=True # override default to connect the gaps
+    ))
+    fig.show()
+   
+for row in session.query(WellMonthRates.date, func.sum(WellMonthRates.oil),
+func.sum(WellMonthRates.gas)).group_by(WellMonthRates.date):
     field_oil.append(row[1])
+    field_gas.append(row[2])
     dates.append(row[0])
 
-fig = go.Figure()
-fig.add_trace(go.Scatter(
-    x=dates,
-    y=field_oil,
-    name = '<b></b> Oil', # Style name/legend entry with html tags
-    connectgaps=True # override default to connect the gaps
-))
-fig.show()
-   
+gaf_creation(dates, field_oil,"Нефть")    
+gaf_creation(dates, field_gas,"Газ") 
+
 
 
 
